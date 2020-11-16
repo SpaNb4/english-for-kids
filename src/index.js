b327d7ca9e9a4217c2f2941df99a774bb1b5b2d2
SpaNb4 <spanb4@gmail.com>
2020-11-16 19:09:34 +0300

    feat: add cards generation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,54 @@
-import './assets/scss/main.scss'
+import './assets/scss/main.scss';
+import cardsArr from './cards';
+
+let cardsDiv = document.querySelector('.cards');
+let cardDiv;
+
+const English = {
+    isStartPage: true,
+    isGameMode: false,
+    isGame: false,
+    wasWrong: 0,
+};
+
+function init() {
+    // set categories 
+    setCards(0);
+}
+
+function clearCards() {
+    for (let i = 0; i < cardDiv.length; i++) {
+        cardDiv[i].remove();
+    }
+}
+
+function setCards(n) {
+    for (let i = 0; i < cardsArr[n].length; i++) {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `<div class="card_img">
+                             <img src="./assets/${cardsArr[n][i].image}" alt="">
+                         </div>
+                         <div class="card_name">${cardsArr[n][i].word}</div>`;
+        cardsDiv.append(card);
+    }
+    cardDiv = document.querySelectorAll('.card');
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    init();
+
+    cardsDiv.addEventListener('click', (e) => {
+        let clickedCard = e.target.closest('.card');
+        if (clickedCard) {
+            let categoriesPos = 0;
+            for (let i = 0; i < cardsArr[0].length; i++) {
+                if (cardsArr[0][i].word == clickedCard.childNodes[2].innerHTML) {
+                    categoriesPos = i + 1;
+                }
+            }
+            clearCards();
+            setCards(categoriesPos);
+        }
+    });
+});
